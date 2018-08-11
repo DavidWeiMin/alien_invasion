9b8a5070b2012ae3286f81a84b88b4bc3bab93a8
dai_wei_min <DavidWeiMin@users.noreply.github.com>
2018-08-11 16:45:07 +0800

    添加音效和游戏按键信息统计

--- scoreboard.py
+++ scoreboard.py
@@ -74,9 +74,9 @@ class Scoreboard():
             self.ships.add(self.ship)
 
     def prep_item(self):
-        '''准备道具图像'''
+        '''准备道具图像'''#todo 道具5图像加入
         self.items = pygame.sprite.Group()
-        item_collection = [self.stats.item_1,self.stats.item_2,self.stats.item_3,self.stats.item_4]
+        item_collection = [self.stats.item_1,self.stats.item_2,self.stats.item_3,self.stats.item_4,self.stats.item_5]
         for kind,number in enumerate(item_collection):
             if len(self.items.sprites()) == 0:
                 if self.stats.ships_left > 0:#alert 如果没有命了就会导致ship对象不存在
